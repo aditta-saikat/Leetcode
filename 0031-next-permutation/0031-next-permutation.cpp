bb35be6d40a868ef9bd108a6a572d7c4b4035c61
Aditta Saikat <94233522+aditta-saikat@users.noreply.github.com>
2024-08-26 00:49:34 +0600

    Time: 7 ms (15.58%), Space: 14.9 MB (5.66%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        next_permutation(nums.begin() , nums.end());
+        
+        for(auto x:nums){
+            cout << x ;
+        }
+        
+    }
+};
