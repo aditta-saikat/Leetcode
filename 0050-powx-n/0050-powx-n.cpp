d0f24083b68e2618290fbe4b8e609afbb2f911c7
Aditta Saikat <94233522+aditta-saikat@users.noreply.github.com>
2024-08-29 00:11:37 +0600

    Time: 0 ms (100.00%), Space: 8.4 MB (14.42%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        double ans = 1;
+        if(x==0 || x==1){
+            return x;
+        }
+        
+        
+        if(n<0){
+            x = 1/x;
+            n = -(n+1);
+            ans *= x;
+        }
+        
+        
+        while(n>0){
+            
+            if(n%2 == 1){
+                ans *= x;
+                n -= 1;
+            }else{
+                n /= 2;
+                x *= x;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
